74b419e7f1675e03a66c6d62ce042c8571c553f8
Carlos Arriagada <carl.arriagadacl@gmail.com>
2023-05-15 06:43:40 -0400

    Agregado comando para correr proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+```sh
+python -m venv venv && source venv/bin/activate && pip install -r requirements.txt && python manage.py makemigrations && python manage.py migrate && python manage.py runserver
+```
--- musicpro/settings.py
+++ musicpro/settings.py
@@ -12,14 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ
-
-# Read .env file
-env = environ.Env()
-environ.Env.read_env()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read .env file
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
